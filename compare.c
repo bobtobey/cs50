ea257669cf1a43be17dfd44e863c86d31f73049e
Bob Tobey <bobtobey@gmail.com>
2024-05-24 00:10:37 +0000

    Thu, May 23, 2024, 8:10 PM -04:00

--- compare.c
+++ compare.c
@@ -8,7 +8,10 @@
     if(x<y){
         printf("x is less than y\n");
     }
+    else if {
+        printf("x is greater than y\n");
+    }
     else {
-        printf("x is not less than y\n");
+        printf(" x is equal to y\n");
     }
  }
