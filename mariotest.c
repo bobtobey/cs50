8c72c783b588826cc6a68803d4f535bdb59987e2
Bob Tobey <bobtobey@gmail.com>
2024-06-05 22:53:58 +0000

    Wed, Jun 5, 2024, 6:53 PM -04:00

---
+++ mariotest.c
@@ -0,0 +1,31 @@
+m#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //declareing the variable
+    int blockheight;
+    do
+    {
+        //assigning the variable for the pyramid height
+        blockheight = get_int("How tall is your pyramid? ");
+    }
+    //checking the variable for a positive integer
+    while(blockheight < 1);
+
+    //first loop creates the n of rows in the pyramid
+    for(int i=0; i < blockheight; i++)
+    {
+        printf("%d ", i);
+        //assigns the variable for # of blocks
+        int blockwidth = i;
+        //second loop creates the n of blocks in each row
+        for(int j=0; j < blockwidth; j++)
+        {
+            string blocks = "#";
+            //printf("%d", j);
+            printf("%s ", blocks);
+        }
+        printf("\n");
+    }
+}
