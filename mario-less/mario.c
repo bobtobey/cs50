ec2cadbf69252b95bf18b9b6d3c2200604e920dc
Bob Tobey <bobtobey@gmail.com>
2024-06-05 22:33:53 +0000

    Wed, Jun 5, 2024, 6:33 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -7,15 +7,14 @@ int main(void)
     int blockheight;
     do
     {
-        //assigning the variable
+        //assigning the variable for the pyramid height
         blockheight = get_int("How tall is your pyramid? ");
     }
     //checking the variable for a positive integer
     while(blockheight < 1);
 
-    int n = blockheight;
     //first loop creates the n of rows in the pyramid
-    for(int i=0; i < n; i++)
+    for(int i=0; i < blockheight; i++)
     {
         printf("%d ", i);
         int blockwidth = i;
