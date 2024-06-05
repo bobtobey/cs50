3a095fe52d2d4781aa0bdc38d3a34dbc1198e3c5
Bob Tobey <bobtobey@gmail.com>
2024-06-05 20:39:33 +0000

    Wed, Jun 5, 2024, 4:39 PM -04:00

--- helloMe.c
+++ helloMe.c
@@ -2,19 +2,11 @@
 #include <stdio.h>
 
 int main(void){
-    //declareing the variable
-    int myheight;
-    do
-    {
-        //assigning the variable
-        myheight = get_int("How tall is your pyramid? ");
+int main() {
+    const int n = 4;  // Define a constant integer n
+    for(int i = 0; i < n; i++) {  // Loop from 0 to n-1
+        printf("%d\n", i);  // Print the value of i followed by a newline
     }
-    //checking the variable for a positive integer
-    while(myheight < 1);
-
-    const int n = 4;
-    for(int i=0; i < n; i++)
-    {
-         printf("%d\n", i);
-    }
+    return 0;  // Return 0 to indicate successful execution
+}
 }
