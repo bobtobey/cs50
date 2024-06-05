cdfa1ad9cde083d9e62ec3ea6f0509532682f0fc
Bob Tobey <bobtobey@gmail.com>
2024-06-05 20:31:21 +0000

    Wed, Jun 5, 2024, 4:31 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -16,7 +16,7 @@ int main(void)
     const int n = myheight;
     for(int i=0; i < n; i++)
     {
-        printf("?");
+        printf(i);
         for(int j=0; j < n; j++)
         {
             printf("#");
