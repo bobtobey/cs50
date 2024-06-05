63fc98a022c9af112a7fedc0da3f7b4b2b6bd6b5
Bob Tobey <bobtobey@gmail.com>
2024-06-05 21:16:05 +0000

    Wed, Jun 5, 2024, 5:16 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -17,7 +17,7 @@ int main(void)
     for(int i=0; i < n; i++)
     {
         printf("%d ", i);
-        for(int j=0; j < n; j--)
+        for(int j=n; j <= n; j--)
         {
             string blocks = "#";
             printf("%s ", blocks);
