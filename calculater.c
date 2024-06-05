09cebab55814ce97cd2536eb112ad88af9290981
Bob Tobey <bobtobey@gmail.com>
2024-06-05 02:46:58 +0000

    Tue, Jun 4, 2024, 10:46 PM -04:00

--- calculater.c
+++ calculater.c
@@ -7,10 +7,10 @@ int main(void){
     int x=get_int("x: ");
     int y=get_int("y: ");
 
-    int z=add();
+    int z=add(x, y);
     printf("%i\n", z);
 }
 
-int add(void){
-    return x + Y;
+int add(int a, int b){
+    return a + b;
 }
