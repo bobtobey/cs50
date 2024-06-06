63051dc2104a9a07d47d98b489d8715d32553193
Bob Tobey <bobtobey@gmail.com>
2024-06-06 15:27:14 +0000

    Thu, Jun 6, 2024, 11:27 AM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -30,7 +30,7 @@ int divide_it(int change)
     // SECOND no remainder = done
     // otherwise take the modulus and * it by first coin value, then divide it by the next highest coin value
     int z = change % 25;
-    printf("%i\n is Z", z);
+    printf("modulus is %i\n", z);
     // repeat
 
     // divide change by coin value .01
