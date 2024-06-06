f083ffe6ced88a5c4931fb944ae93da203213d44
Bob Tobey <bobtobey@gmail.com>
2024-06-06 01:52:57 +0000

    Wed, Jun 5, 2024, 9:52 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,34 +5,34 @@ void buildrow(int blockcount, string blockmaterial);
 
 int main(void)
 {
-    //declareing the variable
+    // declareing the variable
     int blockheight;
     do
     {
-        //assigning the variable for the pyramid height
+        // assigning the variable for the pyramid height
         blockheight = get_int("How tall is your pyramid? ");
     }
-    //checking the variable for a positive integer
-    while(blockheight < 1);
+    // checking the variable for a positive integer
+    while (blockheight < 1);
 
-    //first loop creates the n of rows in the pyramid
-    for(int i=0; i < blockheight; i++)
+    // first loop creates the n of rows in the pyramid
+    for (int i = 0; i < blockheight; i++)
     {
-        //Print row #
-        //printf("%d ", i);
+        // Print row #
+        // printf("%d ", i);
 
-        //build each row with correct amount of spaces and blocks
+        // build each row with correct amount of spaces and blocks
         buildrow(blockheight - i, " ");
-        buildrow(i+1, "#");
+        buildrow(i + 1, "#");
         printf("\n");
     }
 }
 
-//build row function
+// build row function
 void buildrow(int blockcount, string blockmaterial)
 {
-    //second loop creates the n of blocks and spaces in each row
-    for(int j=0; j < blockcount; j++)
+    // second loop creates the n of blocks and spaces in each row
+    for (int j = 0; j < blockcount; j++)
     {
         printf("%s", blockmaterial);
     }
