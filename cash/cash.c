90912f2d684d20a771121428e0df0324f9daa847
Bob Tobey <bobtobey@gmail.com>
2024-06-06 16:14:50 +0000

    Thu, Jun 6, 2024, 12:14 PM -04:00

--- cash/cash.c
+++ cash/cash.c
@@ -28,9 +28,11 @@ int subtract_it(int change)
     int remaining_change;
     // calculate the # of quarters (highest coin value) to give back and subtract it from the change amount
     int quarters = (change / 25);
-    remaining_change = change - quarters;
+    remaining_change = change - (quarters * 25);
+    printf("remaining %i\n", remaining_change);
     // calculate the # of dimes (next highest coin value) to give back and subtract it from the remaining change amount
     int dimes = remaining_change / 10;
+    remaining_change = change - (quarters * 25);
     // calculate the # of nickels (next highest coin value) to give back and subtract it from the change amount
     int nickels = 0;
     // calculate the # of pennies (next highest coin value) to give back and subtract it from the change amount
