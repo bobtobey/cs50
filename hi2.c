68fe9d16444c228514f68d34a60909d33459549b
Bob Tobey <bobtobey@gmail.com>
2024-07-09 06:26:47 +0000

    Tue, Jul 9, 2024, 2:26 AM -04:00

--- hi2.c
+++ hi2.c
@@ -16,4 +16,6 @@ int main(void)
 
     printf("%s\n", words[0]);
     printf("%s\n", words[1]);
+    // this line prints out beyond hi to show the nul value of 0 and the first letter in bye 'B'
+    printf("%i %i %i %i %i\n", words[0][0], words[0][1], words[0][2], words[0][3], words[0][4]);
 }
