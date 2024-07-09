4711117fc688777a52ab1501b12e3f55c454dee5
Bob Tobey <bobtobey@gmail.com>
2024-07-09 05:49:18 +0000

    Tue, Jul 9, 2024, 1:49 AM -04:00

---
+++ hi.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+
+    string s = "HI!";
+
+    printf("%c%c%c\n", c1, c2, c3);
+    printf("%i %i %i\n", c1, c2, c3);
+    printf("%c%c%c\n", s[0], s[1], s[2]);
+    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
+}
