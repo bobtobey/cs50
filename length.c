36c76f2e7e17be635093825fc8bfbbb66ae11077
Bob Tobey <bobtobey@gmail.com>
2024-07-13 18:32:11 +0000

    Sat, Jul 13, 2024, 2:32 PM -04:00

--- length.c
+++ length.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int string_length(string s);
 
@@ -7,7 +8,10 @@ int main(void)
 {
     string name = get_string("Name: ");
     int length = string_length(name);
+    // uses method from string.h to get length
+    int length2 = strlen(name);
     printf("%i\n", length);
+    printf("%i\n", length2);
 
 }
 
