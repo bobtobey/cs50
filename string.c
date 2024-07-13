9e64eee1b1be7674be68d19bbde4757973df3857
Bob Tobey <bobtobey@gmail.com>
2024-07-13 18:44:42 +0000

    Sat, Jul 13, 2024, 2:44 PM -04:00

--- string.c
+++ string.c
@@ -6,7 +6,7 @@ int main(void)
 {
     string s = get_string("Input: ");
     printf("Output: ");
-    for (int i = 0; i < strlen(s); i++)
+    for (int i = 0; length = strlen(s); i < length; i++)
     {
         printf("%c", s[i]);
     }
