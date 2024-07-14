a76fff430e60e2500dfb2d151cecc8ae58f173f5
Bob Tobey <bobtobey@gmail.com>
2024-07-14 23:53:43 +0000

    Sun, Jul 14, 2024, 7:53 PM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,5 +3,9 @@
 
 int main(void)
 {
+    // Prompt players for their scrabble word
+    string player1 = get_string("Player 1 input your word: ");
+    string player2 = get_string("Player 2 input your word: ");
     // place code here
+    printf("Player 1 %s\n", player1);
 }
