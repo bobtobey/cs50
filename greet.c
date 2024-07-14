59326f74fc1ecd5d6c7e3062c74bbc78fecde1fe
Bob Tobey <bobtobey@gmail.com>
2024-07-14 19:40:04 +0000

    Sun, Jul 14, 2024, 3:40 PM -04:00

--- greet.c
+++ greet.c
@@ -5,4 +5,10 @@ int main(int argc, string argv[])
 {
     printf("hello, %s\n", argv[2]);
     printf("Array of words count: %i\n", argc);
+
+    // If you do not know the length of the argv array
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
 }
