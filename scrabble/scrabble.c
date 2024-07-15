56e3288486df17f0989c0ccebb1a6765d1c18eea
Bob Tobey <bobtobey@gmail.com>
2024-07-15 00:24:07 +0000

    Sun, Jul 14, 2024, 8:24 PM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,8 +7,8 @@ int score_words(string word);
 int main(void)
 {
     // Prompt players for their scrabble word
-    string player1word = get_string("Player 1 input your word: ");
-    string player2word = get_string("Player 2 input your word: ");
+    string player1word = get_string("Player 1: ");
+    string player2word = get_string("Player 2: ");
     // score players word
     int player1score = score_words(player1word);
     int player2score = score_words(player2word);
@@ -17,11 +17,11 @@ int main(void)
     printf("Player 1 %s\n", player1word);
     printf("Player 2 %s\n", player2word);
     // compare scores and determine the winner
-    if(player1word < player2word)
+    if(player1score < player2score)
     {
         printf("Player 1 wins!");
     }
-    else if(player1word > player2word)
+    else if(player1score > player2score)
     {
          printf("Player 2 wins!");
     }
