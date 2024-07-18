a54216dc1a51af078741f4dfe650fc6b1d22ea5c
Bob Tobey <bobtobey@gmail.com>
2024-07-18 20:43:25 +0000

    Thu, Jul 18, 2024, 4:43 PM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,6 +10,6 @@ int main(void)
 {
     // Prompt for reading level text sample to review
     string textsample = get_string("Text: ");
-    printf("Text Sample: %s", textsample);
-    printf("Grade)
+    printf("Text Sample: %s\n", textsample);
+    printf("Grade %i\n", gradelevel);
 }
