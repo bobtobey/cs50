09aa860d3bf2a82da5ce8f948d96e3b758da8175
Bob Tobey <bobtobey@gmail.com>
2024-07-18 17:29:20 +0000

    Thu, Jul 18, 2024, 1:29 PM -04:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -46,7 +46,7 @@ int score_words(string word)
 
     // array of letter score values
     int letter_values[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
-     1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+                           1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
     // Loop through arrays
     for (int i = 0; i < word_length; i++)
