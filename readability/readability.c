91c1335a6494d2d066df5882861b5218552e0d1e
Bob Tobey <bobtobey@gmail.com>
2024-07-18 20:52:13 +0000

    Thu, Jul 18, 2024, 4:52 PM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,8 +11,9 @@ int main(void)
     // Prompt for reading level text sample to review
     string textsample = get_string("Text: ");
     printf("Text Sample: %s\n", textsample);
-    // Find formula factors to get the equation index
+    // Find formula factors to get the equation Coleman-Liau: index = 0.0588 * L - 0.296 * S - 15.8
     // get the average number of letters per 100 words
+    
     // get the average number of sentences per 100 words
 
     // Print out the grade level index value for the text provided
