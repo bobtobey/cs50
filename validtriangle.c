e4278d44a55fafebf65b76294fae7076ff87e1e7
Bob Tobey <bobtobey@gmail.com>
2024-07-19 16:33:50 +0000

    Fri, Jul 19, 2024, 12:33 PM -04:00

---
+++ validtriangle.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+ing main(void)
+{
+    // place code here
+}
+
+int valid_triangle(double a, double b, double c)
+{
+
+}
