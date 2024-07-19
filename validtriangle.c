f1d15d831dbb42bb8190416ba89b5393b0f03985
Bob Tobey <bobtobey@gmail.com>
2024-07-19 18:31:20 +0000

    Fri, Jul 19, 2024, 2:31 PM -04:00

--- validtriangle.c
+++ validtriangle.c
@@ -9,13 +9,15 @@ int valid_triangle(double a, double b, double c);
 int main(void)
 {
     // Call Triangle function
-    bool istriangle = valid_triangle(2, 2, 2);
+    bool istriangle = valid_triangle(2, 2, 4);
     // Print result
     printf("Is it a triangle: %i\n", istriangle);
 }
 
+// determine if the 3 lengths form a triangle
 int valid_triangle(double a, double b, double c)
 {
+    // the sum of the lengths of any 2 sides of a triangle must be greater than the length of the third side
     int triange_calc = a + b > c;
     // return result
     return triange_calc;
