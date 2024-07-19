8fcb82bd5d998b37e6cdc95810ea5989c2b238dc
Bob Tobey <bobtobey@gmail.com>
2024-07-19 18:13:26 +0000

    Fri, Jul 19, 2024, 2:13 PM -04:00

--- validtriangle.c
+++ validtriangle.c
@@ -9,10 +9,13 @@ int valid_triangle(double a, double b, double c);
 ing main(void)
 {
     // Call Triangle function
-    int valid_triangle(double a, double b, double c);
+    int istriangle = valid_triangle(2, 2, 2);
+    // Print result
+    printf("Is it a triangle: %b", istriangle);
 }
 
 int valid_triangle(double a, double b, double c)
 {
-
+    // return result
+    return true;
 }
