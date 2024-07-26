75d856b0f62300cdccc1915336f4c8db44271a97
Bob Tobey <bobtobey@gmail.com>
2024-07-26 04:29:10 +0000

    Fri, Jul 26, 2024, 12:29 AM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -15,7 +15,7 @@ int main(int argc, string argv[])
         printf("There is an error with your key, message nummber: %i\n", argc);
         return 1;
     }
-    else if (isdigit(8))
+    else if (check_fordecimals(5))
     {
         // if any character in the command-line argument is not a decimal digit
         printf("Usage: %s key\n", argv[0]);
