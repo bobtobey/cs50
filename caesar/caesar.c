c2893758659718dd2c4b5e11f14c15e31271676f
Bob Tobey <bobtobey@gmail.com>
2024-07-26 03:46:20 +0000

    Thu, Jul 25, 2024, 11:46 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,6 +8,7 @@ int main(int argc, string argv[])
     // error check for a minimum of 2 words typed
     if (argc == 1 || argc > 2)
     {
+        // no command-line argument provided or more than 1
         printf("There is an error with your key, message nummber: %i\n", argc);
         return 1;
     }
