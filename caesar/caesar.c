cd336c89dff8b1d478ddc0e064b03c3b9f177d18
Bob Tobey <bobtobey@gmail.com>
2024-07-31 18:17:50 +0000

    Wed, Jul 31, 2024, 2:17 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -23,7 +23,7 @@ int main(int argc, string argv[])
     }
     else
     {
-        string plaintext = get_string("Enter)
+        string plaintext = get_string("Enter plain text: ");
         printf("Plaintext: %s\n", argv[1]);
         printf("Ciphertext: %s\n", argv[1]);
     }
