1683122d3eca719837f9b6580be520a8163b726c
Bob Tobey <bobtobey@gmail.com>
2024-07-31 22:57:00 +0000

    Wed, Jul 31, 2024, 6:57 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -78,7 +78,9 @@ string encrypt_text(int key, string plaintext)
     for (int j = 0; j < textlength; j++)
     {
         // string x[] = (plaintext[j] + 1);
-        ciphertext[j] = plaintext[j] + 1;
+        ciphertextarray[j] = plaintext[j] + 1;
+        printf("cypher: %c\n", ciphertextarray[j]);
+        
         sum = plaintext[j] + key;
         printf("x = %c and %i\n", sum, sum);
         printf("E Char me %c, %i\n", plaintext[j], plaintext[j]);
