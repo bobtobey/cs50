708d474cf5d06dcba18526f16608bede7d5c9977
Bob Tobey <bobtobey@gmail.com>
2024-07-31 20:56:59 +0000

    Wed, Jul 31, 2024, 4:56 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -71,6 +71,7 @@ string encrypt_text(string key, string plaintext)
     {
         // string x[] = (plaintext[j] + 1);
         sum = plaintext[j] +1;
+        string x += sum;
         printf("x = %c and %i\n", sum, sum);
         printf("E Char me %c, %i\n", plaintext[j], plaintext[j]);
     }
