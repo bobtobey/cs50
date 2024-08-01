30ca2c8ad14441d430e0f80c59dc8725faf06351
Bob Tobey <bobtobey@gmail.com>
2024-08-01 04:03:25 +0000

    Thu, Aug 1, 2024, 12:03 AM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -80,7 +80,7 @@ string encrypt_text(int key, string plaintext)
             // add the cipher key value to each plaintext character
             ciphertext[j] = (plaintext[j] + key);
             printf("Mod %c : %i\n", ((plaintext[j] - 65) + key) % 26, ((plaintext[j] - 65) + key) % 26);
-            printf("Modi %c : %i\n", conversion_index(key, plaintext[j]), conversion_index(key, plaintext[j]);
+            printf("Modi %c : %i\n", conversion_index(key, plaintext[j]), conversion_index(key, plaintext[j]));
             printf("Plain= %c : %i to Cipher= %c : %i\n", plaintext[j], plaintext[j], ciphertext[j], ciphertext[j]);
         }
         else if (isalpha(plaintext[j]) && islower(plaintext[j]))
