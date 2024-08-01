5702693e8ff2a0763c89c9a9aff43b19cad526d5
Bob Tobey <bobtobey@gmail.com>
2024-08-01 02:40:52 +0000

    Wed, Jul 31, 2024, 10:40 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -74,10 +74,11 @@ string encrypt_text(int key, string plaintext)
     // Loop through plaintext and apply caesar key
     for (int j = 0; j < textlength; j++)
     {
+        // formula c[j]=(p[j] + k) % 26
         if (isalpha(plaintext[j]))
         {
             // add the cipher key value to each plaintext character
-            ciphertext[j] = plaintext[j] + key;
+            ciphertext[j] = (plaintext[j] + key);
             printf("Plain= %c : %i to Cipher= %c : %i\n", plaintext[j], plaintext[j], ciphertext[j], ciphertext[j]);
         }
         else
