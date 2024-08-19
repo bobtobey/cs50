820b95bac204cb1c9a464c06e03694fd64e35157
Bob Tobey <bobtobey@gmail.com>
2024-08-19 20:34:31 +0000

    Mon, Aug 19, 2024, 4:34 PM -04:00

--- Lecture2/search.c
+++ Lecture2/search.c
@@ -5,7 +5,7 @@ int main(void)
 {
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
     int n = get_int("Number: ");
-    for(int i = 0; i < numbers; i++)
+    for(int i = 0; i < 7; i++)
     {
         if (numbers[i] == 50)
         {
