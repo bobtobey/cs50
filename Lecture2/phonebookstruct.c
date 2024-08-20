d7dad6d7552fd07a03cc8e7d65721b33b5c9a37f
Bob Tobey <bobtobey@gmail.com>
2024-08-20 19:09:23 +0000

    Tue, Aug 20, 2024, 3:09 PM -04:00

--- Lecture2/phonebookstruct.c
+++ Lecture2/phonebookstruct.c
@@ -14,6 +14,14 @@ int main(void)
     // This now creates 1 array of type person called people
     person people[3];
 
+    // Initialize people - set a person's name and number-- like, how can we access those values
+    person people[0].name = "Carter";
+    person people[0].number = "+1-555-555-5556";
+    person people[1].name = "David";
+    person people[1].number = "+1-555-555-5557";
+    person people[2].name = "John";
+    person people[2].number = "+1-555-555-5558";
+
     string name = get_string("Name: ");
     for (int i = 0; i < 3; i++)
     {
