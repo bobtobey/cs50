ce6549ab0ceeccfbd8a584df0d9d5861f8553517
Bob Tobey <bobtobey@gmail.com>
2024-08-20 18:34:12 +0000

    Tue, Aug 20, 2024, 2:34 PM -04:00

--- Lecture2/phonebook.c
+++ Lecture2/phonebook.c
@@ -5,6 +5,8 @@
 int main(void)
 {
     string names[] = {"Carter", "David", "John"};
+    // In general, the rule of thumb in programming is even if in English we call something a number,
+    // if you wouldn't do math on it ever, you should probably be storing it as a string, not as an integer.
     string numbers[] = {"+1-555-555-5556", "+1-555-555-5557","+1-555-555-5558"};
 
     string name = get_string("Name: ");
