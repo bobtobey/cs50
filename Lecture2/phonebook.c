f9ae4658efd6d7a762bab3a89ca3971261ce8d94
Bob Tobey <bobtobey@gmail.com>
2024-08-20 18:31:09 +0000

    Tue, Aug 20, 2024, 2:31 PM -04:00

--- Lecture2/phonebook.c
+++ Lecture2/phonebook.c
@@ -6,5 +6,16 @@ int main(void)
 {
     string names[] = {"Carter", "David", "John"};
     string numbers[] = {"+1-555-555-5556", "+1-555-555-5557","+1-555-555-5558"};
-    
+
+    string name = get_string("Name: ");
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(names[i], == name) == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not Found\n");
+    return 1;
 }
