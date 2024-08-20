f551ae2d75c597c7285eb24cbe25c026bbb845e2
Bob Tobey <bobtobey@gmail.com>
2024-08-20 18:20:36 +0000

    Tue, Aug 20, 2024, 2:20 PM -04:00

---
+++ Lecture2/phonebook.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string names[] = {"Carter", "David", "John"};
+    string numbers[] = {"+1-555-555-5556", "+1-555-555-5557","+1-555-555-5558"};
+    
+}
