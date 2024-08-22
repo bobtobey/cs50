39505ede8edc61fba7ed62cddd575a466a15fecf
Bob Tobey <bobtobey@gmail.com>
2024-08-22 03:46:03 +0000

    Wed, Aug 21, 2024, 11:46 PM -04:00

---
+++ Lecture2/recursion.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+// similar to mario.c pyramid problem
+
+// initialize functions
+void draw(int n);
+
+int main(void)
+{
+    // get user input for height
+    int height = get_int("Height is: ");
+    // then print the pyramid
+    draw(height);
+}
+
+void draw(int n)
+{
+    // Print pyramid of height n - 1
+    draw(n - 1);
+
+    // first loop provides the height n times/layers
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    // start new layer
+    printf("\n");
+}
