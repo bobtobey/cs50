28670f383a3cb69727a677ba2b3dc8e953b0093d
Bob Tobey <bobtobey@gmail.com>
2024-09-18 15:59:37 +0000

    Wed, Sep 18, 2024, 11:59 AM -04:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -116,7 +116,7 @@ void print_winner(void)
             printf("Total Winner is %s!\n", candidates[j].name);
             return;
         }
-        else if (candidates[j].votes > 0 && candidates[j].votes > currenthighvote)
+        else if (candidates[j].votes > currenthighvote)
         {
             currenthighvote = candidates[j].votes;
             printf("Winner with most votes %s!\n", candidates[j].name);
