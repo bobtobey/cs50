f04e377bb915fbfd21c6f5dbe6e014b660310865
Bob Tobey <bobtobey@gmail.com>
2024-09-28 04:17:13 +0000

    Sat, Sep 28, 2024, 12:17 AM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -214,6 +214,13 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (min == candidates[i].votes)
+        {
+            printf("Tie Me %s at %i\n", candidates[i].name, candidates[i].votes);
+        }
+    }
             printf("Tie\n");
     // return false;
     return true;
