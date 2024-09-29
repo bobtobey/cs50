6ec73d0ab5d62204196ffe80cec96d571af2e16b
Bob Tobey <bobtobey@gmail.com>
2024-09-29 22:07:40 +0000

    Sun, Sep 29, 2024, 6:07 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -252,9 +252,9 @@ void eliminate(int min)
         {
             // elminate me
             printf("Eliminated: %s\n", candidates[i].name);
-            printf("Really Eliminated: %s\n", candidates[i].eliminated);
-            candidates[i].elminated = true;
-            printf("Oh Yes Eliminated: %s\n", candidates[i].eliminated);
+            printf("Really Eliminated: %i\n", candidates[i].eliminated);
+            candidates[i].eliminated = true;
+            printf("Oh Yes Eliminated: %i\n", candidates[i].eliminated);
         }
     }
     return;
