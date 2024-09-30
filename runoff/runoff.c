3687cae979fe8be7b4845d59a2539defb3d6ab9f
Bob Tobey <bobtobey@gmail.com>
2024-09-30 19:34:15 +0000

    Mon, Sep 30, 2024, 3:34 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -203,8 +203,8 @@ int find_min(void)
         if (!candidates[i].eliminated && lowestVoteCount > candidates[i].votes)
         {
             lowestVoteCount = candidates[i].votes;
+            printf("Min Vote Check: %s has %i\n", candidates[i].name, candidates[i].votes);
         }
-        printf("Min Vote Check: %s has %i\n", candidates[i].name, candidates[i].votes);
     }
     printf("lowestvotecount %i\n", lowestVoteCount);
     return lowestVoteCount;
