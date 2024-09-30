1b7a5a510c81cede09629bdedd3d4a1bd0838986
Bob Tobey <bobtobey@gmail.com>
2024-09-30 01:44:17 +0000

    Sun, Sep 29, 2024, 9:44 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -158,7 +158,7 @@ void tabulate(void)
                 printf("candidate name %s has %i votes\n", candidates[preferences[i][j]].name, candidates[preferences[i][0]].votes);
                 // Add up votes for non-eliminated candidates
                 candidates[preferences[i][0]].votes += 1;
-                printf("Tabulate voter %i : Ranks candidate %s with %i votes\n", i, candidates[preferences[i][j]].name, candidates[preferences[i][0]].votes);
+                printf("Tabulate voter %i : Ranks candidate %s with %i votes\n", i, candidates[preferences[i][0]].name, candidates[preferences[i][0]].votes);
             }
         }
     }
