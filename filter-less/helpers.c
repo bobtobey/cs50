b6e272d11d8d16007f26737e3ef36e4d4a46926b
Bob Tobey <bobtobey@gmail.com>
2024-11-12 21:51:25 +0000

    Tue, Nov 12, 2024, 4:51 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            // get average of rgb
+
+            // update pixel values
+        }
+    }
     return;
 }
 
