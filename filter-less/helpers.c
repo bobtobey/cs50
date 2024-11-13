c8f492fe15a39a31b68be6d3bff3a32fab698841
Bob Tobey <bobtobey@gmail.com>
2024-11-13 04:21:25 +0000

    Tue, Nov 12, 2024, 11:21 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -111,7 +111,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             // make a copy of image
             copy[i][j] = image[i][j];
             // Blur pixel values around sample
-            
+            int blur_value = copy[i][j]
 
         }
     }
