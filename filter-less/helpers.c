e804a50ed2b47c0dcf93add2ca2bb1a7619eec9b
Bob Tobey <bobtobey@gmail.com>
2024-11-13 00:10:00 +0000

    Tue, Nov 12, 2024, 7:10 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -40,9 +40,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
             int sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
             int sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
-            printf("Pixel sepiaRed: %i - ", sepiaRed);
+            printf("Sepia R-G-B: %i-%i-%i | ", sepiaRed, sepiaGreen, sepiaBlue);
 
             // update pixel values
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
 
         }
     }
