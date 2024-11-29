2ea786b73ece55c9eef38bff59f62d1f9c908bf6
Bob Tobey <bobtobey@gmail.com>
2024-11-29 19:15:35 +0000

    Fri, Nov 29, 2024, 2:15 PM -05:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -69,7 +69,8 @@ person *create_family(int generations)
         new_person->parent[1] = NULL;
 
         // TODO: Randomly assign alleles
-        new_person->alleles[0] = parent0->alleles
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
