b57bd9c277f9445aeee0419109905a85f3766fa4
Bob Tobey <bobtobey@gmail.com>
2024-11-29 22:46:57 +0000

    Fri, Nov 29, 2024, 5:46 PM -05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -46,11 +46,12 @@ bool load(const char *dictionary)
     }
 
     char word[LENGTH + 1];
-    debug
+
     // Read words from the file
     while (fscanf(input, "%s", word) != EOF)
     {
         // TODO: Insert the word into your data structure
+        printf("File opened successfully.\n");
         printf("%s\n", word);
     }
 
