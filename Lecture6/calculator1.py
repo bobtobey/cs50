dcf91839d8f0bfa2803e90923fe4200eac985b38
Bob Tobey <bobtobey@gmail.com>
2024-11-30 21:26:05 +0000

    Sat, Nov 30, 2024, 4:26 PM -05:00

--- Lecture6/calculator1.py
+++ Lecture6/calculator1.py
@@ -1,5 +1,7 @@
 from cs50
 
+# this example is to show that you can import a library and
+# prefix it to the function to avoid naming collisions in this program = cs50.get_int
 x = cs50.get_int("x: ")
 y = cs50.get_int("y: ")
 
