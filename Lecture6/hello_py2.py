72d3968523956aee6af4f9d6a2326e3eaaa244b6
Bob Tobey <bobtobey@gmail.com>
2024-11-30 19:15:46 +0000

    Sat, Nov 30, 2024, 2:15 PM -05:00

--- Lecture6/hello_py2.py
+++ Lecture6/hello_py2.py
@@ -1,7 +1,7 @@
 answer = get_string("What is your name? ")
 # this version concatenates with the normal + symbol to join the strings
 print("hello," + answer)
-# this version lets python provide the space needed between strings itself and uses a ',' comma to apply multiple 
+# this version lets python provide the space needed between strings itself and uses a ',' comma to apply multiple arguments
 print("hello,", answer)
 # this version uses a format string by prefixes 'f' at the start and using { braces } inside
 print(f"hello, {answer}")
