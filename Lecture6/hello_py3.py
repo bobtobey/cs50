2b827adb417246b9853da1c69c25cfc603e00d6e
Bob Tobey <bobtobey@gmail.com>
2024-11-30 19:50:25 +0000

    Sat, Nov 30, 2024, 2:50 PM -05:00

---
+++ Lecture6/hello_py3.py
@@ -0,0 +1,8 @@
+
+answer = input("What is your name? ")
+# this version concatenates with the normal + symbol to join the strings
+print("hello, " + answer)
+# this version lets python provide the space needed between strings itself and uses a ',' comma to apply multiple arguments
+print("hello,", answer)
+# this version uses a format string by prefixes 'f' at the start and using { braces } inside
+print(f"hello, {answer}")
