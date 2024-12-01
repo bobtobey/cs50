7c7707b10639c119bc53918d5897e274f33fe370
Bob Tobey <bobtobey@gmail.com>
2024-12-01 07:36:34 +0000

    Sun, Dec 1, 2024, 2:36 AM -05:00

--- Lecture6/uppercase3.py
+++ Lecture6/uppercase3.py
@@ -1,3 +1,4 @@
 # this function provides an alternative improvement on uppercase2.py
 before = input("Before: ")
+# you can put short bits of code inside of format strings { curly braces }
 print(f"After: {before.upper()}")
