e3ec3613b3dcc07bdb5042eb74e55beabc4e4d38
Bob Tobey <bobtobey@gmail.com>
2024-12-01 19:47:11 +0000

    Sun, Dec 1, 2024, 2:47 PM -05:00

--- Lecture6/calculater-decimals.py
+++ Lecture6/calculater-decimals.py
@@ -5,4 +5,4 @@ y = int(input("y: "))
 # use f string to configure the variable to print out to a specific number of digits
 # to print out 50 decimal points use a dot after a colon and specify the number of digits then an f to make clear it's a float.
 z = x / y
-print(f{z:.50f})
+print(f"{z:.50f}")
