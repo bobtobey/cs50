5678b9330191d961304bafd6c1d3bf65056dd7a8
Bob Tobey <bobtobey@gmail.com>
2024-12-01 19:17:14 +0000

    Sun, Dec 1, 2024, 2:17 PM -05:00

--- Lecture6/calculator-trunc.py
+++ Lecture6/calculator-trunc.py
@@ -2,5 +2,8 @@
 x = int(input("x: "))
 y = int(input("y: "))
 
+# type in 1 for x, 3 for y
+# it didn't truncate Python is smarter at converting one value to another
+# an integer divided by an integer ends up giving you a fractional component
 z = x / y
 print(z)
