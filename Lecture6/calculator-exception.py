7b527e42ae5ffb14577b4aed4a5917a084f6f4bd
Bob Tobey <bobtobey@gmail.com>
2024-12-01 20:07:20 +0000

    Sun, Dec 1, 2024, 3:07 PM -05:00

--- Lecture6/calculator-exception.py
+++ Lecture6/calculator-exception.py
@@ -1,5 +1,6 @@
 # example of how to handle exceptions
 def get_int(prompt):
+    # loop until you get a response that does not error out
     while True:
         try:
             return int(input(prompt))
