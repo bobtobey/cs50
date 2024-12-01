6b83fe6913839d71cdaf8515bc9795cbb5f23fab
Bob Tobey <bobtobey@gmail.com>
2024-12-01 04:07:38 +0000

    Sat, Nov 30, 2024, 11:07 PM -05:00

---
+++ Lecture6/agree1.py
@@ -0,0 +1,7 @@
+s = input("Do you agree? ")
+
+# string comparison test with multiple conditions using an array LIST
+if s in ["Y","y", "yes"]:
+    print("Agreed")
+elif s in ["N", "n", "no"]:
+    print("Not Agreed")
