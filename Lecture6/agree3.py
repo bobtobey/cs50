eb796fff6dd518f61627b8c10dea86a4e9a4a579
Bob Tobey <bobtobey@gmail.com>
2024-12-01 05:12:51 +0000

    Sun, Dec 1, 2024, 12:12 AM -05:00

--- Lecture6/agree3.py
+++ Lecture6/agree3.py
@@ -1,6 +1,6 @@
 # reset all inputs to uppercase to handle diff use cases
 # and this chains the string method to end of the variable declaration
-s = input("Do you agree? ").upper();
+s = input("Do you agree? ").upper()
 
 # string comparison test with multiple conditions using an array LIST
 if s in ["y", "yes"]:
