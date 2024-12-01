0eaf0f58bfa9e849acb9f78ac1aa4e6537803dd9
Bob Tobey <bobtobey@gmail.com>
2024-12-01 17:27:50 +0000

    Sun, Dec 1, 2024, 12:27 PM -05:00

--- Lecture6/meow2.py
+++ Lecture6/meow2.py
@@ -1,11 +1,11 @@
 # defines a main function
 def main():
-
-# his defines a (def) python function
-def meow():
     # this is a python loop
     for i in range(3):
         meow()
+
+# his defines a (def) python function
+def meow():
     print("meow 2nd gen!!")
 
 # call main function at the end of program
