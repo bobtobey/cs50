9e03375de06363924919ac373fc8086d80fd7759
Bob Tobey <bobtobey@gmail.com>
2024-12-01 06:21:44 +0000

    Sun, Dec 1, 2024, 1:21 AM -05:00

--- Lecture6/uppercase.py
+++ Lecture6/uppercase.py
@@ -1,8 +1,9 @@
 # this function changes the input to all uppercase
 before = input("Before: ")
-print("After: ", end="")
+# adding the 'end=""' parameter is defaulted to end="\n"
+print("After: ", end="\n")
 for c in before:
     # adding the 'end=""' paramenter allows you to determine
     print(c.upper(), end="")
-# 
+#
 print()
