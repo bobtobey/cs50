c4063d05b4b8279c610b455f1a6971fa6aa22f80
Bob Tobey <bobtobey@gmail.com>
2024-12-01 17:15:33 +0000

    Sun, Dec 1, 2024, 12:15 PM -05:00

---
+++ Lecture6/meow2.py
@@ -0,0 +1,12 @@
+# main
+def main():
+
+# python function
+def meow():
+    # python loop for Meow
+    for i in range(3):
+        print("meow 1st gen!")
+        meow()
+    print("meow 2nd gen!!")
+
+main()
