a66e3988065cf807dad5fbce7c123fc5e5dcb5d2
Bob Tobey <bobtobey@gmail.com>
2024-12-01 19:44:33 +0000

    Sun, Dec 1, 2024, 2:44 PM -05:00

---
+++ Lecture6/calculater-decimals.py
@@ -0,0 +1,8 @@
+# example for setting the number decimal places to show in python
+x = int(input("x: "))
+y = int(input("y: "))
+
+# use f string to configure the variable to print out to a specific number of digits
+# to print out 50 decimal points use a dot after a colon and specify the number of digits then an f to make clear it's a float.
+z = x / y
+print(f{z:.50f})
