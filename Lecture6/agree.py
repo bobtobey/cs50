b5f91dcfb99db16a5ff7605bd156b87bafed81cc
Bob Tobey <bobtobey@gmail.com>
2024-12-01 04:04:17 +0000

    Sat, Nov 30, 2024, 11:04 PM -05:00

--- Lecture6/agree.py
+++ Lecture6/agree.py
@@ -1,6 +1,6 @@
 s = input("Do you agree? ")
 
-# string comparison test
+# string comparison test with multiple conditions
 if s == "Y" or s == "y":
     print("Agreed")
 elif s == "N" or "s" == "n":
