8e2fbd5bec769483f1647699cc53282bfa49da15
Bob Tobey <bobtobey@gmail.com>
2024-12-02 01:38:08 +0000

    Sun, Dec 1, 2024, 8:38 PM -05:00

--- Lecture6/mario3.py
+++ Lecture6/mario3.py
@@ -1,4 +1,10 @@
 # Mario revisited: print out a single row of bricks
 for i in range(3):
     # use named parameter end="" to create row
-    print("#", end)
+    print("#", end="")
+    # Alternative way to acheive same result
+    # * means multiply the string by itself 4x
+    print("#" * 4)
+
+# create new line
+print()
