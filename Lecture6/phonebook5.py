cca4aed24dc94d793418df6ae83e64652f04038f
Bob Tobey <bobtobey@gmail.com>
2024-12-02 07:36:40 +0000

    Mon, Dec 2, 2024, 2:36 AM -05:00

--- Lecture6/phonebook5.py
+++ Lecture6/phonebook5.py
@@ -1,13 +1,15 @@
 # Implements a phone book using a dictionary
 from cs50 import get_string
 
+# converts this from a list of dictionaries to just one big dictionary
+# but it is more restrictive to just one key/value pair in this case
 people = {
     "Carter": "+1-617-495-1000",
     "David": "+1-617-495-1000",
     "John": "+1-949-468-2750",
 }
 
-# Search for name
+# and it is simpler to Search for name
 name = get_string("Name: ")
 if name in people:
     print(f"Number: {people[name]}")
