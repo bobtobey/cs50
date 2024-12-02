c659ea5e55ab913a757c0b3c8835b9118c8db42d
Bob Tobey <bobtobey@gmail.com>
2024-12-02 04:35:38 +0000

    Sun, Dec 1, 2024, 11:35 PM -05:00

--- Lecture6/scores-lists2.py
+++ Lecture6/scores-lists2.py
@@ -1,9 +1,12 @@
+# Use an empty list to store user scores and get average
 from cs50 import get_int
 
 scores = []
 for i in range(3):
     score = get_int("Score: ")
     scores.append(score)
+    # alternate syntax
+    # scores = scores + [score]
 
 average = sum(scores) / len(scores)
 print(f"Average: {average}")
