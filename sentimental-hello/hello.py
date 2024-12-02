b1cea590169c24e4de0e1b7559cab158cb772dc4
Bob Tobey <bobtobey@gmail.com>
2024-12-02 18:06:06 +0000

    Mon, Dec 2, 2024, 1:06 PM -05:00

--- sentimental-hello/hello.py
+++ sentimental-hello/hello.py
@@ -4,14 +4,11 @@
 def get_string(prompt):
 
     while True:
-        try:
-            name = input(prompt)
-            if len(name) > 0:
-                return name
-            else:
-                print("Please enter a name with at least 1 character.")
-        except ValueError:
-            pass
+        name = input(prompt)
+        if len(name) > 0:
+            return name
+        else:
+            print("Please enter a name with at least 1 character.")
 
 def main():
     # prompt user for name
