244e34a513fcae7cb239e671ed998a9e3241e2e1
Bob Tobey <bobtobey@gmail.com>
2024-12-02 18:36:03 +0000

    Mon, Dec 2, 2024, 1:36 PM -05:00

--- sentimental-hello/mario.py
+++ sentimental-hello/mario.py
@@ -9,7 +9,7 @@ def get_int(prompt):
             if n > 0:
                 return n
             else:
-                print("Please enter a positive integer")
+                print("Please enter a positive integer between 1 and 8.")
         except ValueError:
             pass
 
