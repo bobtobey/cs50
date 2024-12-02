c21b5bf59501edda34cf08665480b22438cea6e1
Bob Tobey <bobtobey@gmail.com>
2024-12-02 18:32:49 +0000

    Mon, Dec 2, 2024, 1:32 PM -05:00

--- sentimental-hello/mario.py
+++ sentimental-hello/mario.py
@@ -2,8 +2,10 @@
 # define a prompt function
 def get_int(prompt):
     while True:
+        # test for a non-numeric answer
         try:
             n = int(input(prompt))
+            # test for positive integer
             if n > 0:
                 return n
             else:
