0742204673bb97f8aff8a965bd0ca295e35665fc
Bob Tobey <bobtobey@gmail.com>
2024-12-04 06:38:38 +0000

    Wed, Dec 4, 2024, 1:38 AM -05:00

--- Lecture6/greet1.py
+++ Lecture6/greet1.py
@@ -1,6 +1,6 @@
 from sys import argv
 
-# how to test for a command line argument and exit with code
+# how to test for a command line argument and use it for expression
 if len(argv) > 2:
     print(f"Hello, {argv[1]}.")
 else:
