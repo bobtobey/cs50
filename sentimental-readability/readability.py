9a44c18aecee492c62022921db57d46958b38a3f
Bob Tobey <bobtobey@gmail.com>
2024-12-04 02:20:43 +0000

    Tue, Dec 3, 2024, 9:20 PM -05:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -45,7 +45,7 @@ def main():
     # calculate grade index #
     grade_index = calc_grade_index(text_value)
     # determine text grade level
-    if grade_index< 1:
+    if grade_index < 1:
         print(f"Grade {grade_index}")
     elif grade_index >= 16:
         print(f"Grade {grade_index}")
