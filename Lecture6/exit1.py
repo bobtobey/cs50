e1ee01e4f0860e63f470f8d469697aa841e263ac
Bob Tobey <bobtobey@gmail.com>
2024-12-04 06:33:37 +0000

    Wed, Dec 4, 2024, 1:33 AM -05:00

--- Lecture6/exit1.py
+++ Lecture6/exit1.py
@@ -4,3 +4,6 @@ if len(sys.argv) != 2:
     print("Missing command-line argument.")
     # used to exit a program with an exit/error code #
     sys.exit(1)
+
+print(f"Hello, {sys.argv[1]}")
+sys.exit(0)
