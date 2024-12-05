5d734da5fb9d1fcfa95cccc4381502c12fdd2354
Bob Tobey <bobtobey@gmail.com>
2024-12-05 18:37:29 +0000

    Thu, Dec 5, 2024, 1:37 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Check command-line arguments
+    if (argc != 2)
+    {
+        printf("Please enter 1 argument.\n");
+        return 1;
+    }
+    else
+    {
+        printf("Command line success\n");
+    }
+
+
 }
