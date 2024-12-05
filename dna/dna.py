21244cb47f0b2410277f08f2b393a7bb607568d9
Bob Tobey <bobtobey@gmail.com>
2024-12-05 02:20:31 +0000

    Wed, Dec 4, 2024, 9:20 PM -05:00

--- dna/dna.py
+++ dna/dna.py
@@ -19,7 +19,7 @@ def main():
     with open(db_file) as file:
         reader = csv.DictReader(file)
         strs = reader.fieldnames[1:]
-        # print(reader.fieldnames)
+        print(f"test {reader.fieldnames}")
         for row in reader:
             rows.append(row)
             print(row)
