28703e3ec13a1d4ceccc677815152867c6db1881
Bob Tobey <bobtobey@gmail.com>
2024-12-09 21:57:30 +0000

    Mon, Dec 9, 2024, 4:57 PM -05:00

---
+++ Lecture6/dictionary.py
@@ -0,0 +1,10 @@
+# Creates a dictionary of pizza toppings
+pizzas = {
+    "cheese": 9,
+    "pepperoni": 10,
+    "veggetable": 11,
+    "black olive": 12
+}
+# Loop through keys
+for pie in pizzas:
+    print(price)
--- Lecture6/tuples-loop.py
+++ Lecture6/tuples-loop.py
@@ -8,11 +8,3 @@ presidents = [
 # Create a for loop
 for prez, year in presidents:
     print("In {1}, {0} took office".format(prez, year))
-
-# Creates a dictionary of pizza toppings
-pizzas = {
-    "cheese": 9,
-    "pepperoni": 10,
-    "veggetable": 11,
-    "black olive": 12
-}
