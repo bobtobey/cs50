a6d4252aafa53e4d1720f8a735bb089716db67b9
Bob Tobey <bobtobey@gmail.com>
2024-12-11 03:41:42 +0000

    Tue, Dec 10, 2024, 10:41 PM -05:00

--- Lecture7/favorite_csv2.py
+++ Lecture7/favorite_csv2.py
@@ -1,4 +1,11 @@
 import csv
 
-# 'with' keyword automatically closes the file 
+# 'with' keyword automatically closes the file outside the 'with' block
 with open("people-100.csv", "r") as file:
+    # reader library comes with Python
+    reader = csv.reader(file)
+    # next() allows us to skip the header row of a csv file
+    next(reader)
+    for row in reader:
+        # this will print out the 2nd row
+        print(row[1])
