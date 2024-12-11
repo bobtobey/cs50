a305071b95606779b6facbd845e9dd834696afe6
Bob Tobey <bobtobey@gmail.com>
2024-12-11 05:06:07 +0000

    Wed, Dec 11, 2024, 12:06 AM -05:00

--- Lecture7/favorites_csv3.py
+++ Lecture7/favorites_csv3.py
@@ -1,7 +1,7 @@
 import csv
 
 with open("people-100.csv", "r") as file:
-    # reader library comes with Python
+    # DictReader library comes with Python
     reader = csv.DictReader(file)
     for row in reader:
         # this will print out the 2nd row
