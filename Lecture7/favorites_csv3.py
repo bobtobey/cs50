d153fe13162aa43c810707695fcacd6e34e97070
Bob Tobey <bobtobey@gmail.com>
2024-12-11 05:10:19 +0000

    Wed, Dec 11, 2024, 12:10 AM -05:00

--- Lecture7/favorites_csv3.py
+++ Lecture7/favorites_csv3.py
@@ -2,8 +2,8 @@ import csv
 
 with open("people-100.csv", "r") as file:
     # DictReader analyzes the header row of the file and figures out the columns
-    # and this now creates a dictionary 
     reader = csv.DictReader(file)
+    # this iterates ober reader to create a dictionary
     for row in reader:
         # this will print out the 2nd row
         print(row["Email"])
