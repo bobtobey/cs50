41441994b1fee0e0f83a5e015f91cc0d292fcd74
Bob Tobey <bobtobey@gmail.com>
2024-12-11 05:08:40 +0000

    Wed, Dec 11, 2024, 12:08 AM -05:00

--- Lecture7/favorites_csv3.py
+++ Lecture7/favorites_csv3.py
@@ -1,7 +1,8 @@
 import csv
 
 with open("people-100.csv", "r") as file:
-    # DictReader library comes with Python
+    # DictReader analyzes the header row of the file and figures out the columns
+    # and this now creates a dictionary 
     reader = csv.DictReader(file)
     for row in reader:
         # this will print out the 2nd row
