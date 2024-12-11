f5427a3966c26712da7ec4f467339e77320855c7
Bob Tobey <bobtobey@gmail.com>
2024-12-11 04:02:27 +0000

    Tue, Dec 10, 2024, 11:02 PM -05:00

--- Lecture7/favorites_csv1.py
+++ Lecture7/favorites_csv1.py
@@ -1,8 +1,8 @@
 import csv
 
 file = open("people-100.csv", "r")
-    # reader library comes with Python
-    reader = csv.reader(file)
-    # next() allows us to skip the header row of a csv file
-    next(reader)
+# reader library comes with Python
+reader = csv.reader(file)
+# next() allows us to skip the header row of a csv file
+next(reader)
 close(file)
