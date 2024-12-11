ece417c5c035466fa16c84836d4ce4b82b58d0e0
Bob Tobey <bobtobey@gmail.com>
2024-12-11 04:48:13 +0000

    Tue, Dec 10, 2024, 11:48 PM -05:00

--- Lecture7/favorites_csv3.py
+++ Lecture7/favorites_csv3.py
@@ -2,7 +2,7 @@ import csv
 
 with open("people-100.csv", "r") as file:
     # reader library comes with Python
-    reader = DictReader(file)
+    reader = csv.DictReader(file)
     for row in reader:
         # this will print out the 2nd row
         print(row["Email"])
