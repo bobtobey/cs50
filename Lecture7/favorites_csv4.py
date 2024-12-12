217a4ffe4244a86fd5d6c82d8d78181b59f0c8ba
Bob Tobey <bobtobey@gmail.com>
2024-12-12 01:46:06 +0000

    Wed, Dec 11, 2024, 8:46 PM -05:00

--- Lecture7/favorites_csv4.py
+++ Lecture7/favorites_csv4.py
@@ -4,12 +4,14 @@ with open("people-100.csv", "r") as file:
     # DictReader analyzes the header row of the file and figures out the columns
     reader = csv.DictReader(file)
     # Initialize counter variables
-    # scratch = 0
-    # c = 0
-    # python = 0
+    # male = 0
+    # female = 0
     # Cleaner way to initialize multiple variables
-    scratch, c, python = 0, 0, 0
+    male, female = 0, 0, 0
 
     for row in reader:
-        f = row["Email"]
-        if f == "
+        f = row["Sex"]
+        if f == "Male":
+            male += 1
+        elif f == "Female":
+            female += 1
