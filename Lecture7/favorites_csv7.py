47d140e0ff12c766c47458b444ebc228ffcadaea
Bob Tobey <bobtobey@gmail.com>
2024-12-12 16:14:40 +0000

    Thu, Dec 12, 2024, 11:14 AM -05:00

--- Lecture7/favorites_csv7.py
+++ Lecture7/favorites_csv7.py
@@ -1,5 +1,7 @@
 import csv
 
+from collections inport Counter
+
 with open("people-100.csv", "r") as file:
     # DictReader analyzes the header row of the file and figures out the columns
     reader = csv.DictReader(file)
