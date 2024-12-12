7596ed324115032cfe623e8df8304d91ae2b8676
Bob Tobey <bobtobey@gmail.com>
2024-12-12 17:04:49 +0000

    Thu, Dec 12, 2024, 12:04 PM -05:00

---
+++ Lecture7/favorites_interactive.py
@@ -0,0 +1,19 @@
+import csv
+
+# import Counter method
+from collections import Counter
+
+with open("people-100.csv", "r") as file:
+    # DictReader analyzes the header row of the file and figures out the columns
+    reader = csv.DictReader(file)
+    # creates an object with counting capabilities built in
+    counts = Counter()
+
+    # grab the favorite sex from that row
+    for row in reader:
+        # index into that counter using favorite and increment by 1
+        favorite = row["Sex"]
+        counts[favorite] += 1
+
+favorite = input("Favorite: ")
+print(f"{favorite}: {counts[favorite]}")
