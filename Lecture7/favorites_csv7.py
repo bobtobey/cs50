b2edf9555047a0527be7a37e0872023957b0ba7c
Bob Tobey <bobtobey@gmail.com>
2024-12-12 16:16:16 +0000

    Thu, Dec 12, 2024, 11:16 AM -05:00

--- Lecture7/favorites_csv7.py
+++ Lecture7/favorites_csv7.py
@@ -5,14 +5,11 @@ from collections inport Counter
 with open("people-100.csv", "r") as file:
     # DictReader analyzes the header row of the file and figures out the columns
     reader = csv.DictReader(file)
-    counts = {}
+    counts = Counter()
 
     for row in reader:
         f = row["Sex"]
-        if f in counts:
-            counts[f] += 1
-        else:
-            counts[f] = 1
+        counts[f] += 1
 
 # Sort by values
 for f in sorted(counts, key=counts.get, reverse=True):
