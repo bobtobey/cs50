c51bef6a7fe62aa248e3af9269bb49d6a09373f4
Bob Tobey <bobtobey@gmail.com>
2024-12-12 05:07:05 +0000

    Thu, Dec 12, 2024, 12:07 AM -05:00

--- Lecture7/favorites_csv5.py
+++ Lecture7/favorites_csv5.py
@@ -1,3 +1,16 @@
-people-100.csv", "r") as file:
+import csv
+
+with open("people-100.csv", "r") as file:
     # DictReader analyzes the header row of the file and figures out the columns
     reader = csv.DictReader(file)
+    counts = {}
+
+    for row in reader:
+        f = row["Sex"]
+        if f in counts:
+            counts[f] += 1
+        else:
+            counts[f] = 1
+
+for f in counts:
+    print(f"{f}: {counts[f]}")
