adb03d28002cf284bbc4eb74b7cbd2bddccf4aa8
Bob Tobey <bobtobey@gmail.com>
2024-12-16 02:31:04 +0000

    Sun, Dec 15, 2024, 9:31 PM -05:00

--- Lecture7/favorits_sql.py
+++ Lecture7/favorits_sql.py
@@ -6,6 +6,6 @@ db = SQL("sqlite:///favorites.db")
 # Create a terminal input
 favorite = input("Favorite: ")
 
-# exec
+# execute an SQL query that will create an 
 rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favorite)
 row = rows[0]
