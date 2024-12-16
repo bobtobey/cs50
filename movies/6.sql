daaa7d169366cb2063c0e9ccdbda8c4831f2f03f
Bob Tobey <bobtobey@gmail.com>
2024-12-16 19:53:55 +0000

    Mon, Dec 16, 2024, 2:53 PM -05:00

--- movies/6.sql
+++ movies/6.sql
@@ -2,4 +2,4 @@ SELECT AVG(rating) FROM ratings;
 
 SELECT title, year FROM movies WHERE year = 2012;
 
-SELECT AVG(rating) FROM ratings WHERE 
+SELECT AVG(rating) FROM ratings WHERE rating IN (SELECT year FROM movies WHERE year = 2012);
