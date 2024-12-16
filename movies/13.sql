6c7d945582a770a6fb9b71bb47d12eacb4f19497
Bob Tobey <bobtobey@gmail.com>
2024-12-16 23:58:29 +0000

    Mon, Dec 16, 2024, 6:58 PM -05:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,8 +2,8 @@
 -- SELECT name, birth, id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958;
 
 -- find movie id's with Kevin Bacon's id
--- SELECT id FROM movies AS m WHERE m.id IN
--- (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Kevin Bacon' AND birth = 1958);
+SELECT id FROM movies AS m WHERE m.id IN
+(SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Kevin Bacon' AND birth = 1958);
 
 -- find people with id's associated with those movie id's
 SELECT name FROM people WHERE id IN
