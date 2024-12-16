cf5f8b6449cf16cb51149fd43b2b399c7c4e9dfd
Bob Tobey <bobtobey@gmail.com>
2024-12-16 02:32:55 +0000

    Sun, Dec 15, 2024, 9:32 PM -05:00

--- Lecture7/favorits_sql.py
+++ Lecture7/favorits_sql.py
@@ -6,6 +6,9 @@ db = SQL("sqlite:///favorites.db")
 # Create a terminal input
 favorite = input("Favorite: ")
 
-# execute an SQL query that will create an 
+# execute an SQL query that will create a temporary table that we store in a variable
 rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favorite)
+# Get the first row in our temp table
 row = rows[0]
+
+print(row["n"])
