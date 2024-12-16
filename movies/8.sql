a797e084864ac90c87b72750f25a44a94dfa5f8f
Bob Tobey <bobtobey@gmail.com>
2024-12-16 20:58:26 +0000

    Mon, Dec 16, 2024, 3:58 PM -05:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,3 @@
-SELECT name FROM people AS p JOIN movies AS m WHERE 
+SELECT name FROM people AS p JOIN movies AS m ON p.id = m.id WHERE m.title = 'Toy Story';
+
+SELECT title FROM movies WHERE title = 'Toy Story';
