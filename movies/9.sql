ecffab624fdd3793a034d6b844d5c09c7d37910c
Bob Tobey <bobtobey@gmail.com>
2024-12-16 21:44:05 +0000

    Mon, Dec 16, 2024, 4:44 PM -05:00

--- movies/9.sql
+++ movies/9.sql
@@ -8,3 +8,5 @@ SELECT DISTINCT(name) FROM people WHERE id IN (SELECT year FROM movies WHERE yea
 SELECT name, COUNT(*) AS c FROM people WHERE id IN
 (SELECT person_id FROM stars
 WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004));
+
+SELECT name, year FROM people JOIN stars ON peo
