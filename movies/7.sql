31ee3c7226bcd97fca3491c1fa69467e166610af
Bob Tobey <bobtobey@gmail.com>
2024-12-16 20:50:52 +0000

    Mon, Dec 16, 2024, 3:50 PM -05:00

--- movies/7.sql
+++ movies/7.sql
@@ -3,3 +3,5 @@ SELECT title FROM movies WHERE year = 2010 LIMIT 12;
 SELECT rating FROM ratings ORDER BY rating DESC LIMIT 12;
 
 SELECT title, rating FROM movies AS m JOIN ratings AS r ON m.id = r.movie_id WHERE year = 2010 ORDER BY rating DESC, title ASC;
+
+SELECT title, rating FROM movies AS m JOIN ratings AS r ON m.id = r.movie_id WHERE year = 2010, rating IS NOT NULL ORDER BY rating DESC, title ASC;
