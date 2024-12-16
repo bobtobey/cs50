ce9ad687863d68448627e84ad6ee19d82c8ec1cc
Bob Tobey <bobtobey@gmail.com>
2024-12-16 17:39:41 +0000

    Mon, Dec 16, 2024, 12:39 PM -05:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,3 +1,3 @@
-SELECT name, artist_id FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = 'Post Malone' AND id = 54);
+SELECT name, energy, artist_id FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = 'Drake' AND id = 23);
 
 SELECT name, id FROM artists WHERE name = 'Drake';
