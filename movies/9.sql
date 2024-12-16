b607c1ad74553069049c355be4996417a2555ffc
Bob Tobey <bobtobey@gmail.com>
2024-12-16 21:46:33 +0000

    Mon, Dec 16, 2024, 4:46 PM -05:00

--- movies/9.sql
+++ movies/9.sql
@@ -9,4 +9,4 @@ SELECT name, COUNT(*) AS c FROM people WHERE id IN
 (SELECT person_id FROM stars
 WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004));
 
-SELECT name, year FROM people JOIN stars ON peo
+SELECT name, year FROM people JOIN stars ON people.id = stars.person_id JOIN movies on stars.movie_Id = movie.id WHERE year = 2004 ORDER BY year;
