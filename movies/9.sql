2f708d70b9dd9f5c9fc8686994629cfdae673185
Bob Tobey <bobtobey@gmail.com>
2024-12-16 21:19:50 +0000

    Mon, Dec 16, 2024, 4:19 PM -05:00

--- movies/9.sql
+++ movies/9.sql
@@ -3,3 +3,5 @@ SELECT DISTINCT(name) FROM people ORDER BY birth LIMIT 10;
 SELECT year, title FROM movies WHERE year = 2004 LIMIT 10;
 
 SELECT DISTINCT(name) FROM people WHERE id IN (SELECT year FROM movies WHERE year = 2004);
+
+SELECT name FROM people WHERE id IN (SELECT year FROM movies WHERE year = 2004);
