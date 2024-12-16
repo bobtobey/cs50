e16efb2ea888cbe7d81c990eaff3ed53cb6e931b
Bob Tobey <bobtobey@gmail.com>
2024-12-16 23:12:36 +0000

    Mon, Dec 16, 2024, 6:12 PM -05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,4 @@
+-- use the first WHERE clause to check for actor 1 and use a 2nd WHERE clause to check for actress 2
 SELECT title FROM movies AS m
 WHERE m.id IN (Select movie_id FROM stars AS s JOIN people AS p1 ON s.person_id = p1.id WHERE name = 'Bradley Cooper')
 AND m.id IN (Select movie_id FROM stars AS s JOIN people AS p1 ON s.person_id = p1.id WHERE name = 'Jennifer Lawrence');
