21e7fec15c077560d8a73524dce46a5d6dd5bdbc
Bob Tobey <bobtobey@gmail.com>
2024-12-16 23:09:23 +0000

    Mon, Dec 16, 2024, 6:09 PM -05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,7 @@
 SELECT title FROM movies AS m
+WHERE m.id IN (Select movie_id FROM stars AS s JOIN people AS p1 ON s.person_id = p1.id WHERE name = 'Bradley Cooper')
+AND m.id IN (Select movie_id FROM stars AS s JOIN people AS p1 ON s.person_id = p1.id WHERE name = 'Jennifer Lawrence');
+
+SELECT title FROM movies AS m
 JOIN stars AS s ON m.id = s.movie_id
 JOIN people AS p ON s.person_id = p.id WHERE name = 'Bradley Cooper' AND name = 'Jennifer Lawrence';
