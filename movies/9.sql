fd4924674ee7468fc56037d41ca8a950873a52be
Bob Tobey <bobtobey@gmail.com>
2024-12-16 21:35:21 +0000

    Mon, Dec 16, 2024, 4:35 PM -05:00

--- movies/9.sql
+++ movies/9.sql
@@ -5,6 +5,6 @@ SELECT year, title FROM movies WHERE year = 2004 LIMIT 10;
 SELECT DISTINCT(name) FROM people WHERE id IN (SELECT year FROM movies WHERE year = 2004);
 
 -- find id for movies in 2004 first, then find the person id for stars in those movies, last find the name id of those
-SELECT name, year FROM people WHERE id IN
+SELECT name FROM people WHERE id IN
 (SELECT person_id FROM stars
 WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004) ORDER BY year);
