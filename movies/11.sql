899f26b414867640ac84a228907fdd4af092bd3e
Bob Tobey <bobtobey@gmail.com>
2024-12-16 22:25:18 +0000

    Mon, Dec 16, 2024, 5:25 PM -05:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,4 +2,6 @@ SELECT title FROM movies LIMIT 5;
 
 SELECT name, id FROM people WHERE name = 'Chadwick Boseman';
 
-SELECT title, rating FROM movies AS m JOIN ratings AS r ON m.id = r.movie_id ORDER BY rating DESC LIMIT 5;
+SELECT title, rating FROM movies AS m JOIN ratings AS r ON m.id = r.movie_id
+WHERE 
+ORDER BY rating DESC LIMIT 5;
