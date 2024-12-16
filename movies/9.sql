f1fa0397224b5e9a74f1559c8d3aceb499ee83cb
Bob Tobey <bobtobey@gmail.com>
2024-12-16 21:59:50 +0000

    Mon, Dec 16, 2024, 4:59 PM -05:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,20 +1,3 @@
-SELECT DISTINCT(name) FROM people ORDER BY birth LIMIT 10;
-
-SELECT year, title FROM movies WHERE year = 2004 LIMIT 10;
-
-SELECT DISTINCT(name) FROM people WHERE id IN (SELECT year FROM movies WHERE year = 2004);
-
 -- find id for movies in 2004 first, then find the person id for stars in those movies, last find the name id of those
-SELECT name, COUNT(*) AS c FROM people WHERE id IN
-(SELECT person_id FROM stars
-WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004));
-
-SELECT DISTINCT(name), birth, year FROM people JOIN stars ON people.id = stars.person_id
+SELECT DISTINCT(name) FROM people JOIN stars ON people.id = stars.person_id
 JOIN movies on stars.movie_Id = movies.id WHERE year = 2004 ORDER BY birth LIMIT 30;
-
-SELECT name, year, ROW_NUMBER() OVER (ORDER BY birth) AS row_num
-FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE year = 2004
-ORDER BY birth;
