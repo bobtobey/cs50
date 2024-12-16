41dc91d0db99c22c63bfef1cccf3ae90ec6af5b1
Bob Tobey <bobtobey@gmail.com>
2024-12-16 18:05:39 +0000

    Mon, Dec 16, 2024, 1:05 PM -05:00

--- songs/3.sql
+++ songs/3.sql
@@ -1,3 +1,1 @@
-SELECT name, duration_ms FROM songs WHERE duration_ms > 0 DESC LIMIT 5;
-
-SELECT name, duration_ms FROM songs ORDER BY DESC LIMIT 5;
+SELECT name, duration_ms FROM songs ORDER BY duration_ms DESC LIMIT 5;
