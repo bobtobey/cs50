9b499ee7c0cfafc0849978a334c5723e9c9d6095
Bob Tobey <bobtobey@gmail.com>
2024-12-16 17:43:31 +0000

    Mon, Dec 16, 2024, 12:43 PM -05:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,3 +1,1 @@
 SELECT AVG(energy) FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = 'Drake' AND id = 23);
-
-SELECT name, id FROM artists WHERE name = 'Drake';
---
+++ songs/8.sql
@@ -0,0 +1,3 @@
+SELECT name FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = 'Drake' AND id = 23);
+
+SELECT name, id FROM artists WHERE name = 'Drake';
