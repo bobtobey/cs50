2b55920da8ebb41204b63f068e568311b3992714
Bob Tobey <bobtobey@gmail.com>
2024-12-16 17:29:40 +0000

    Mon, Dec 16, 2024, 12:29 PM -05:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,1 +1,3 @@
-SELECT name, artist_id FROM songs WHERE artist_id IN (SELECT id, name FROM songs LIMIT 10;
+SELECT name, artist_id FROM songs WHERE artist_id IN (SELECT id, name FROM artists WHERE artists = 'Post Malone' LIMIT 10);
+
+SELECT 
