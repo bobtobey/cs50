bf41ddfe09ce049a2acf14ad2f1c48a8506b40c1
Bob Tobey <bobtobey@gmail.com>
2024-12-16 17:14:14 +0000

    Mon, Dec 16, 2024, 12:14 PM -05:00

--- songs/4.sql
+++ songs/4.sql
@@ -1,1 +1,1 @@
-SELECT name, danceability, energy, valence FROM songs WHERE danceability > 0.75 AND energy > 0.75 AND valence > 0.75;
+SELECT name FROM songs WHERE danceability > 0.75 AND energy > 0.75 AND valence > 0.75;
