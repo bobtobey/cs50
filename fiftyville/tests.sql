edbf5d5710082374b2432a83f61c61fc9c65d0ea
Bob Tobey <bobtobey@gmail.com>
2024-12-17 23:18:36 +0000

    Tue, Dec 17, 2024, 6:18 PM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -64,14 +64,14 @@ SELECT DISTINCT 'temp_people_table' AS table_name
     ,p.passport_number, bsl.license_plate
     ,ppl.name AS rec_name, pc.receiver AS rec_num
     -- ,psgr2.passport_number
-    -- ,tbt.person_id AS bank_id, tbt.account_number
+    ,tbt.person_id AS bank_id, tbt.account_number
 FROM temp_people_table AS tpt
     JOIN phone_calls AS pc ON tpt.phone_number = pc.caller
     JOIN people AS ppl ON pc.receiver = ppl.phone_number
     JOIN bakery_security_logs AS bsl ON tpt.license_plate = bsl.license_plate
     JOIN passengers AS p ON tpt.passport_number = p.passport_number
     -- JOIN passengers AS psgr2 ON ppl.passport_number = psgr2.passport_number
-    JOIN temp_bank_table AS tbt ON ppl.id = tbt.person_id
+    JOIN temp_bank_table AS tbt ON tpt.id = tbt.person_id
 -- RESULTS: there are 5 matching names with atm transactions and phone calls
         WHERE pc.year = 2023 AND pc.month = 7 AND pc.day = 28 AND pc.duration < 60
 -- RESULTS: there are 3 matching names with license plate
