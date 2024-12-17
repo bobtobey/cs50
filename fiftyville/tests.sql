28b7170223897dc506ecd2737003448003fcc8eb
Bob Tobey <bobtobey@gmail.com>
2024-12-17 05:08:46 +0000

    Tue, Dec 17, 2024, 12:08 AM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -18,7 +18,9 @@ SELECT account_number, person_id FROM bank_accounts
         WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw') ORDER BY account_number;
 -- ACTION: find persons name from bank account
 SELECT name, id FROM people
-    WHERE id IN (SELECT person_id FROM bank_accounts)
+    WHERE id IN (SELECT person_id FROM bank_accounts
+        WHERE account_number IN (SELECT account_number FROM atm_transactions
+            WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw') ORDER BY account_number;)
 
 -- Review phone calls from 10:15am to 10:30am
 SELECT * FROM phone_calls WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60;
