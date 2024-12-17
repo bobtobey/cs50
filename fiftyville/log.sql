ea45d3a6d9aaeac60c82857324645a25b65a274d
Bob Tobey <bobtobey@gmail.com>
2024-12-17 02:37:48 +0000

    Mon, Dec 16, 2024, 9:37 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,4 +7,5 @@ SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND 
 -- ACTION: Next read interviews for that date of the crime where witness mentions bakery
 SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 -- RESULT: witness Ruth sees thief leave in a car at 10:25am from bakery parking lot
--- RESULT: witness Eugene recognizes the thief from that morning before 10:15am, in line at the Legget St. ATM
+-- RESULT: witness Eugene recognizes the thief from that morning before 10:15am, withdrawing money at the Leggett St. ATM
+-- RESULT: witness Raymond saw the thief call someone right after the crime
