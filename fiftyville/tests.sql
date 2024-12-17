ab238b8c5c87acf6fa149154883fbd5dbbf17043
Bob Tobey <bobtobey@gmail.com>
2024-12-17 05:16:07 +0000

    Tue, Dec 17, 2024, 12:16 AM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -16,7 +16,8 @@ SELECT * FROM atm_transactions
 SELECT account_number, person_id FROM bank_accounts
     WHERE account_number IN (SELECT account_number FROM atm_transactions
         WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw') ORDER BY account_number;
--- ACTION: find persons name from bank account
+-- ACTION: find persons name from bank account and store them in a temp table
+CREATE TEMPORARY TABLE temp_suspect_table AS
 SELECT name, id FROM people WHERE id IN (
     SELECT person_id FROM bank_accounts
         WHERE account_number IN (
