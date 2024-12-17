b55a52625aadb2c2cb9e1fd568ad941d48ca1afc
Bob Tobey <bobtobey@gmail.com>
2024-12-17 02:26:15 +0000

    Mon, Dec 16, 2024, 9:26 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- ACTION: Read crime report for that date and on that street
 SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
-5 crime events happened on the date of the theft
+-- RESULT: the crime happended at 10:15am in the bakery, there are 3 witness interviews that mention the bakery
 
--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -1,4 +1,6 @@
--- Read crime report for that date and on that street
-SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
--- the crime happended at 10:15am in the bakery, there are 3 witness interviews that mention the bakery
+-- ACTION: Read crime report for that date and on that street
+-- SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+-- RESULT: the crime happended at 10:15am in the bakery, there are 3 witness interviews that mention the bakery
 
+-- Next read interviews for that date of the crime
+SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
