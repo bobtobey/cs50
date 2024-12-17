6c35983ba355ef13f5f4ce469bcb001c47343cbd
Bob Tobey <bobtobey@gmail.com>
2024-12-17 05:55:13 +0000

    Tue, Dec 17, 2024, 12:55 AM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -18,7 +18,7 @@ SELECT account_number, person_id FROM bank_accounts
         WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw') ORDER BY account_number;
 -- ACTION: find persons name from bank account and store them in a temp table
 CREATE TEMPORARY TABLE temp_people_table AS
-SELECT name, id, phone_number FROM people WHERE id IN (
+SELECT name, id, phone_number, passport_number, license_plate FROM people WHERE id IN (
     SELECT person_id FROM bank_accounts
         WHERE account_number IN (
             SELECT account_number FROM atm_transactions
