7f36ae83f6698482ae2f8383aeaf713f5cf7db07
Bob Tobey <bobtobey@gmail.com>
2024-12-17 06:49:20 +0000

    Tue, Dec 17, 2024, 1:49 AM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -2,16 +2,19 @@
 SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- RESULT: the crime happended at 10:15am in the bakery, there are 3 witness interviews that mention the bakery
 
--- Next read interviews for that date of the crime where witness mentions bakery
+-- ACTION: Next read interviews for that date of the crime where witness mentions bakery
 SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 
--- Review bakery security logs for 10:15am to 10:30am
+-- ACTION: Review bakery security logs for 10:15am to 10:30am
 SELECT hour, activity, license_plate FROM bakery_security_logs
     WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND activity LIKE 'exit' ORDER BY license_plate;
+-- RESULT: 9 vehicles exited the bakery parking lot at 10 with plate info
 
--- Review atm transactions from 10:14am or earlier at Leggett st.
+-- ACTION: Review atm transactions from 10:14am or earlier at Leggett st.
 SELECT * FROM atm_transactions
     WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw' ORDER BY account_number;
+-- RESULT: 8 atm withdrawls were made on Leggett St.
+
 -- ACTIONS: Cross check atm records for crime location and date with Bank records
 SELECT account_number, person_id FROM bank_accounts
     WHERE account_number IN (SELECT account_number FROM atm_transactions
