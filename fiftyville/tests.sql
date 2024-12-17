91fdbb5066ab5111b59d65e268ca96c638ce9ab2
Bob Tobey <bobtobey@gmail.com>
2024-12-17 05:40:40 +0000

    Tue, Dec 17, 2024, 12:40 AM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -30,8 +30,10 @@ SELECT * FROM temp_people_table;
 -- Review phone calls from 10:15am to 10:30am
 SELECT * FROM phone_calls WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60;
 -- ACTION: cross check phone numbers with temp people table
-SELECT temp_people_table.name, id, phone_number FROM temp_people_table
-    JOIN phone_calls ON temp_people_table.id = phone_numbers.person_id;
+
+
+-- SELECT temp_people_table.name, id, phone_number FROM temp_people_table
+--     JOIN phone_number ON temp_people_table.id = phone_numbers.person_id;
 
 -- Review earliest flights leaving on 7/29 and Airport id's
 SELECT * FROM flights WHERE year = 2023 AND month = 7 AND day = 29 ORDER BY hour, minute;
