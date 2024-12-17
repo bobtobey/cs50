13dc105397526f3758bd17f17b2ffbc54992fa4f
Bob Tobey <bobtobey@gmail.com>
2024-12-17 02:32:05 +0000

    Mon, Dec 16, 2024, 9:32 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,3 +4,6 @@
 SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- RESULT: the crime happended at 10:15am in the bakery, there are 3 witness interviews that mention the bakery
 
+-- ACTION: Next read interviews for that date of the crime where witness mentions bakery
+SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
+-- RESULT: witness Ruth sees thief leave in a car at 10:25am
--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -1,6 +1,6 @@
 -- ACTION: Read crime report for that date and on that street
--- SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- RESULT: the crime happended at 10:15am in the bakery, there are 3 witness interviews that mention the bakery
 
--- Next read interviews for that date of the crime
-SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
+-- Next read interviews for that date of the crime where witness mentions bakery
+SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
