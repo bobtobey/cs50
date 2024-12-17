c0beb237225c1301dc539ef778f090eb38ea4ce8
Bob Tobey <bobtobey@gmail.com>
2024-12-17 23:52:48 +0000

    Tue, Dec 17, 2024, 6:52 PM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -95,10 +95,13 @@ AND p.flight_id = 36;
 SELECT * FROM people LIMIT 5;
 
 -- DROP TEMPORARY TABLE IF EXISTS temp_people;
-SELECT pc.caller AS caller_num, p1.name AS caller_name,
-       pc.receiver AS receiver_num, p2.name AS receiver_name
-FROM phone_calls AS pc
-    JOIN people AS p1 ON pc.caller = p1.phone_number
-    JOIN people AS p2 ON pc.receiver = p2.phone_number
-WHERE pc.year = 2023 AND pc.month = 7 AND pc.day = 28 AND pc.duration < 60;
+-- Query to check flight passengers
+SELECT passengers.passport_number, people.name
+FROM passengers
+JOIN flights ON passengers.flight_id = flights.id
+JOIN people ON passengers.passport_number = people.passport_number
+WHERE flights.destination_airport_id = (
+    SELECT id FROM airports WHERE city = 'New York City'
+)
+AND flights.year = 2021 AND flights.month = 7 AND flights.day = 29;
 
