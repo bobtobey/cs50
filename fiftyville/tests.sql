04c2bd3537761678ff99697d7e3a8b708417a007
Bob Tobey <bobtobey@gmail.com>
2024-12-17 16:36:48 +0000

    Tue, Dec 17, 2024, 11:36 AM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -20,6 +20,7 @@ CREATE TEMPORARY TABLE temp_bank_table AS
 SELECT account_number, person_id FROM bank_accounts
     WHERE account_number IN (SELECT account_number FROM atm_transactions
         WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw') ORDER BY account_number;
+SELECT * FROM temp_bank_table;
 -- RESULTS: found the matching person id's for the 8 atm transactions yields 8 names
 
 -- ACTION: find persons name from bank account and store them in a temp table
@@ -53,16 +54,17 @@ SELECT * FROM passengers WHERE flight_id = 36;
 -- RESULTS: 8 persons found
 
 -- ACTION: cross check phone numbers and license plates with temp people table
-SELECT 'temp_people_table' AS table_name,
-    tpt.name, tpt.id, pc.caller,
-    ppl.name, pc.receiver,
-    p.passport_number, bsl.license_plate,
-    ba.person_id AS bank_id, ba.account_number
+SELECT 'temp_people_table' AS table_name
+    ,tpt.name, tpt.id, pc.caller
+    ,ppl.name, pc.receiver
+    ,p.passport_number, bsl.license_plate
+    ,tbt.person_id AS bank_id, tbt.account_number
 FROM temp_people_table AS tpt
     JOIN phone_calls AS pc ON tpt.phone_number = pc.caller
     JOIN people AS ppl ON pc.receiver = ppl.phone_number
     JOIN bakery_security_logs AS bsl ON tpt.license_plate = bsl.license_plate
     JOIN passengers AS p ON tpt.passport_number = p.passport_number
+    JOIN temp_bank_table AS tbt ON ppl.id = tbt.person_id
 -- RESULTS: there are 5 matching names with atm transactions and phone calls
         WHERE pc.year = 2023 AND pc.month = 7 AND pc.day = 28 AND pc.duration < 60
 -- RESULTS: there are 3 matching names with license plate
