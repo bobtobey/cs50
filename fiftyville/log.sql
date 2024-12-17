1e104a559147d834ab8c66f6e624a12f141a7140
Bob Tobey <bobtobey@gmail.com>
2024-12-17 01:18:34 +0000

    Mon, Dec 16, 2024, 8:18 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT * FROM crime_scene_reports
+SELECT * FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28;
 
