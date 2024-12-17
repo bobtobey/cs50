b05e62ba361bd11c121b11a1e7dd284505d727c7
Bob Tobey <bobtobey@gmail.com>
2024-12-17 15:14:08 +0000

    Tue, Dec 17, 2024, 10:14 AM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -36,6 +36,10 @@ SELECT * FROM temp_people_table;
 SELECT * FROM phone_calls WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60;
 -- RESULT: 9 calls were made on this date that lasted less than 1 min. yields 9 callers/receivers
 
+-- ACTION: Match names to call & receivers
+SELECT caller, receiver FROM phone_calls
+    JOIN people ON phone_calls.receiver = people.phone_number
+
 -- Review earliest flights leaving on 7/29 and Airport id's
 SELECT * FROM flights WHERE year = 2023 AND month = 7 AND day = 29 ORDER BY hour, minute;
 SELECT * FROM airports;
