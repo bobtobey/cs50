c942b2a2b433e08dd3de5717f8ce7599a7231a6d
Bob Tobey <bobtobey@gmail.com>
2024-12-17 03:42:43 +0000

    Mon, Dec 16, 2024, 10:42 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -20,4 +20,6 @@ SELECT hour, activity, license_plate FROM bakery_security_logs
 SELECT * FROM atm_transactions WHERE year = 2023 AND month = 7 AND day = 28;
 -- RESULT: 8 atm withdrawls were made on Leggett St.
 
-
+-- ACTION: Review phone calls from 10:15am to 10:30am
+SELECT * FROM phone_calls WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60;
+-- RESULT: 9 calls were made on this date that lasted less than 1 min.
