158f47c82551ddd427993692ffe3e6e4205b8432
Bob Tobey <bobtobey@gmail.com>
2024-12-17 02:21:18 +0000

    Mon, Dec 16, 2024, 9:21 PM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -1,2 +1,3 @@
 -- Read crime report for that date and on that street
 SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+-- the crime happended at 10:15am in the bakery, there are 3 witness interviews
