442cdb0075010a395da21f882e7ad10baac92ba3
Bob Tobey <bobtobey@gmail.com>
2024-12-17 22:24:35 +0000

    Tue, Dec 17, 2024, 5:24 PM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -17,6 +17,7 @@ SELECT * FROM atm_transactions
 -- RESULT: 8 atm withdrawls were made on Leggett St. yielding 8 account_numbers
 
 -- ACTION: Cross check Bank records against atm records for crime location and date similarities
+CREATE TEMPORARY TABLE temp_bank_table AS
 SELECT ba.account_number, ba.person_id FROM bank_accounts AS ba
     JOIN atm_transactions AS atm ON ba.account_number = atm.account_number
         WHERE year = 2023 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw' ORDER BY ba.account_number;
