9e17189138e94f8e32bcf2449c6f8c7f699f7e44
Bob Tobey <bobtobey@gmail.com>
2024-12-18 00:47:57 +0000

    Tue, Dec 17, 2024, 7:47 PM -05:00

--- fiftyville/tests.sql
+++ fiftyville/tests.sql
@@ -1,3 +1,5 @@
+-- Test log of any SQL queries you execute as you solve the mystery.
+
 -- ACTION: Read crime report for that date and on that street
 SELECT description FROM crime_scene_reports WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- RESULT: the crime happended at 10:15am in Emma's bakery, there are 3 witness interviews that mention the bakery
