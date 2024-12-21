8c6a22d1045fab20fec5bab3877dad8192e4e752
Bob Tobey <bobtobey@gmail.com>
2024-12-21 03:18:41 +0000

    Fri, Dec 20, 2024, 10:18 PM -05:00

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -1,11 +1,11 @@
 // Universal Event Listener
 document.addEventListener('DOMContentLoaded', function() {
-    // Select the buttons and the dialog using querySelector
+    // initialize varibles for dialog and dialog buttons
     const toggleButton = document.querySelector("#open-article-image-bloc1");
     const dialogBloc = document.querySelector("#article-image-bloc1");
     const closeButton = document.querySelector("#article-image-bloc1 .btn-primary");
 
-    // Function to toggle visibility
+    // Toggle Dialog Display
     function toggleDialog() {
         if (dialogBloc.style.display === "none" || dialogBloc.style.display === "") {
             dialogBloc.style.display = "block";
