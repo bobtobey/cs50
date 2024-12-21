08bdb88cf74a4fd90e58a7bb219255eacfc99704
Bob Tobey <bobtobey@gmail.com>
2024-12-21 02:35:18 +0000

    Fri, Dec 20, 2024, 9:35 PM -05:00

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -1,4 +1,28 @@
 
+
+// Toggle Dialog Display
+function dialogToggle(dialog, dialogCaller) {
+    // Get current display state
+    const currentState = window.getComputedStyle(dialog).display;
+
+    if (currentState === 'none') {
+        // Store the original border color if not already stored
+        if (!dialog.dataset.originalBorderColor) {
+            dialog.dataset.originalBorderColor = window.getComputedStyle(dialog).borderColor;
+        }
+
+        dialog.style.display = 'block';
+        dialog.setAttribute('data-dialog-state', 'true');
+        dialogCaller.classList.remove('btn-target-off');
+        dialogCaller.classList.add('btn-target-on');
+    } else {
+        dialog.style.display = 'none';
+        dialog.setAttribute('data-dialog-state', 'false');
+        dialog.style.borderColor = dialog.dataset.originalBorderColor; // Reset to original border color when closing
+        dialogCaller.classList.remove('btn-target-on');
+        dialogCaller.classList.add('btn-target-off');
+    }
+}
 // Universal Event Listener
 document.addEventListener('DOMContentLoaded', function() {
     // Document listeners
