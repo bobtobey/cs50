a3d0ff8db00249c7d2fd752e6414869309f1a728
Bob Tobey <bobtobey@gmail.com>
2024-12-23 17:30:15 +0000

    Mon, Dec 23, 2024, 12:30 PM -05:00

---
+++ Lecture9/hello5/app.py
@@ -0,0 +1,15 @@
+# Says hello to request.args["name"]
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+#  this route just serves index.html
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# creating a second route
+@app.route("/greet")
+def greet():
+    name = request.args.get("name", "world")
+    return render_template("greet.html", name=name)
