9edefd11f55f692d27652c915612f00370124880
Bob Tobey <bobtobey@gmail.com>
2024-12-24 17:43:25 +0000

    Tue, Dec 24, 2024, 12:43 PM -05:00

--- Lecture9/login/app.py
+++ Lecture9/login/app.py
@@ -5,6 +5,7 @@ from flask_session import Session
 app = Flask(__name__)
 
 # Configure session
+# copy|paste to get sessions working in flask
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
