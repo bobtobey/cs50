21eb296c66f3f8ceffc7aac5f86348cc6fbafc0e
Bob Tobey <bobtobey@gmail.com>
2024-12-24 17:30:31 +0000

    Tue, Dec 24, 2024, 12:30 PM -05:00

---
+++ Lecture9/login/app2.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, render_template, request, session
+
+# Configure app
+app = Flask(__name__)
+
+# copy|paste to get sessions working in flask
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
