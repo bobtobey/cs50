53d3d6551f7f9334498654766b0ae083a74948a8
Bob Tobey <bobtobey@gmail.com>
2024-12-24 05:52:04 +0000

    Tue, Dec 24, 2024, 12:52 AM -05:00

--- Lecture9/froshim/app.py
+++ Lecture9/froshim/app.py
@@ -5,7 +5,4 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        name = request.form.get("name", "world")
-        return render_template("greet.html", name=name)
     return render_template("index.html")
