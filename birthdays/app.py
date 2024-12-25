cfae6cc34a3a59d0d8484641e0f3993ac579cd07
Bob Tobey <bobtobey@gmail.com>
2024-12-25 01:47:14 +0000

    Tue, Dec 24, 2024, 8:47 PM -05:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -30,7 +30,7 @@ def index():
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
-        # Remember registrant
+        # Insert Name and Birthday to db
         db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
         return redirect("/")
 
