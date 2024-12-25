bce8b52198d8edd3c299abcaede932dd0429ec50
Bob Tobey <bobtobey@gmail.com>
2024-12-25 01:24:59 +0000

    Tue, Dec 24, 2024, 8:24 PM -05:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -24,6 +24,13 @@ def after_request(response):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+
+        return redirect("/")
+
+    else:
 
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
