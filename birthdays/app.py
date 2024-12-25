a67f9e22001f6281cbf37c8d843f089e0376a79f
Bob Tobey <bobtobey@gmail.com>
2024-12-25 02:12:08 +0000

    Tue, Dec 24, 2024, 9:12 PM -05:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -28,11 +28,7 @@ def index():
 
         # TODO: Add the user's entry into the database
         name = request.form.get("name")
-        if not name:
-            return redirect("/")
-        try:
-            name = isalpha(name)
-        except ValueError:
+        if not name or not name.isalpha():
             return redirect("/")
 
         month = request.form.get("month")
