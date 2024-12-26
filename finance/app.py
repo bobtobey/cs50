41e855c7d605b13120ce62c922831b4df5755059
Bob Tobey <bobtobey@gmail.com>
2024-12-26 18:21:29 +0000

    Thu, Dec 26, 2024, 1:21 PM -05:00

--- finance/app.py
+++ finance/app.py
@@ -134,12 +134,12 @@ def register():
             "SELECT * FROM users WHERE username = ?", request.form.get("username")
         )
         # Ensure username exists and password is correct
-        if len(rows) != 1):
-            print(rows)
+        if len(rows) != 1:
+            print(len(rows))
             return apology("invalid username and/or password", 403)
         else:
             print("JOE")
-            
+
         # Generate hash for password
         hash = generate_password_hash(password)
 
