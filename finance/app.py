44abb946fed13a589b69dbfb94f20009bcc83fd1
Bob Tobey <bobtobey@gmail.com>
2024-12-26 22:01:47 +0000

    Thu, Dec 26, 2024, 5:01 PM -05:00

--- finance/app.py
+++ finance/app.py
@@ -68,14 +68,11 @@ def buy():
 
         # Check database for username
         moneyavailable = db.execute(
-            "SELECT * FROM users, WHERE username = session["user_id"]"
-        )
-        moneyavailable = db.execute(
             "SELECT cash FROM users WHERE id = ?", session["user_id"]
         )
         # Ensure username exists
-        # if len(moneyavailable) != 0:
-        #     return apology("username already exists", 403)
+        if len(moneyavailable) != 0:
+            return apology("username already exists", 403)
 
         print(f"User Id: {moneyavailable}")
         print(f"$: {symbol}")
