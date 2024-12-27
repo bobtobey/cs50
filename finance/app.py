b14919615a8e8bc5c6ae431990240643f19c5ffa
Bob Tobey <bobtobey@gmail.com>
2024-12-27 06:13:16 +0000

    Fri, Dec 27, 2024, 1:13 AM -05:00

--- finance/app.py
+++ finance/app.py
@@ -39,8 +39,12 @@ def index():
     # SQL select all transactions from DB
     transactions = db.execute("SELECT * FROM transactions")
     print(transactions)
+    
+    # get db column names from table
+    headers = db.execute("PRAGMA table_info(transactions)")
+    headers = [header["name"] for header in headers]
     # pass birthdays into the index.html
-    return render_template("index.html", transactions=transactions)
+    return render_template("index.html", transactions=transactions, headers=headers)
 
 
 @app.route("/buy", methods=["GET", "POST"])
